9157cc9e7df0f86ccfac7129603873089292ab4f
sarajoha <sjcc333@gmail.com>
2019-03-03 15:56:04 -0500

    Added js file to manage clicks on buttons

---
+++ myscript.js
@@ -0,0 +1,7 @@
+document.getElementById("bt-slack1").addEventListener("click", goToWeb);
+
+document.getElementById("bt-slack2").addEventListener("click", goToWeb);
+
+function goToWeb() {
+   return window.location = "https://chingu-prework.slack.com/oauth/559848241895.f52724d042497f7b19823cac31934ffb5e367deac87d7309b116299316741ff5"
+}
